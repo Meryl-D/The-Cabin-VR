78ec2c6cde6385c1c64fab49189e4a170f5dbcfb
Meryl Dubois <meryl.dubois@heig-vd.ch>
2023-02-22 11:27:55 +0100

    initial commit

--- src/components/TheCameraRig.vue
+++ src/components/TheCameraRig.vue
@@ -11,43 +11,68 @@
     movement-controls="camera: #head;"
     disable-in-vr="component: movement-controls;"
   >
-
+    <a-entity
+      id="head"
+      look-controls="pointerLockEnabled: true"
+      simple-navmesh-constraint="navmesh: [data-role='nav-mesh']; height: 1.65;"
+      disable-in-vr="component: simple-navmesh-constraint;"
+      camera
+      position="0 1.65 0"
+    > 
+      <!-- Flashlight and light -->
       <a-entity
-        id="head"
-        look-controls="pointerLockEnabled: true"
-        simple-navmesh-constraint="navmesh: [data-role='nav-mesh']; height: 1.65;"
-        disable-in-vr="component: simple-navmesh-constraint;"
-        camera
-        position="0 1.65 0"
+        hide-in-vr
+        gltf-model="../assets/simple_flashlight.glb"
+        position="0.2 -0.3 -0.3"
+        rotation="0 180 0"
       >
         <a-entity
-          geometry="primitive: circle; radius: 0.0003;"
-          material="shader: flat; color: white;"
-          cursor
-          raycaster="far: 2; objects: [clickable]; showLine: false;"
-          position="0 0 -0.1"
-          disable-in-vr="component: raycaster; disableInAR: false;"
-          hide-in-vr="hideInAR: false"
+          rotation="0 180 0"
+          light="type: spot; angle: 30; penumbra: 1; intensity: 1; decay: 2; distance: 15;"
         ></a-entity>
       </a-entity>
 
+      <!-- Pointer -->
       <a-entity
-        id="hand-left"
-        hand-controls="hand: left"
-        blink-controls="
-          cameraRig: #camera-rig;
-          teleportOrigin: #head;
-          collisionEntities: [data-role='nav-mesh'];
-          snapTurn: false;
-        "
+        id="pointer"
+        geometry="primitive: circle; radius: 0.0003;"
+        material="shader: flat; color: white;"
+        cursor
+        raycaster="far: 2; objects: [clickable]; showLine: false;"
+        position="0 0 -0.1"
+        disable-in-vr="component: raycaster; disableInAR: false;"
+        hide-in-vr="hideInAR: false"
       ></a-entity>
+    </a-entity>
 
+    <a-entity
+      id="hand-left"
+      hand-controls="hand: left; handModelStyle: lowPoly; color: #ffcccc"
+      blink-controls="
+        cameraRig: #camera-rig;
+        teleportOrigin: #head;
+        collisionEntities: [data-role='nav-mesh'];
+        snapTurn: false;
+      "
+    ></a-entity>
+
+    <a-entity
+      id="hand-right"
+      hand-controls="hand: right; handModelStyle: lowPoly; color: #ffcccc"
+      laser-controls="hand: right"
+      raycaster="far: 2; objects: [clickable]; showLine: true;"
+    >
       <a-entity
-        id="hand-right"
-        hand-controls="hand: right"
-        laser-controls="hand: right"
-        raycaster="far: 2; objects: [clickable]; showLine: true;"
-      ></a-entity>
+          gltf-model="../assets/simple_flashlight.glb"
+          position="-0.06 -0.12 -0.15"
+          rotation="0 180 0"
+        >
+          <a-entity
+            rotation="0 180 0"
+            light="type: spot; angle: 30; penumbra: 1; intensity: 1; decay: 2; distance: 15;"
+          ></a-entity>
+        </a-entity>
+    </a-entity>
 
   </a-entity>
 </template>
--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -1,85 +1,84 @@
 <script setup>
-  import { ref } from 'vue';
-  import { randomHsl } from '../utils/color.js';
+import { ref } from 'vue';
 
-  import BoxColorChanging from './BoxColorChanging.vue';
-  import TheCameraRig from './TheCameraRig.vue';
-  import TheNavMesh from './TheNavMesh.vue';
+import TheCameraRig from './TheCameraRig.vue';
+import TheNavMesh from './TheNavMesh.vue';
 
-  import '../aframe/life-like-automaton';
+import '../aframe/clickable';
 
-  defineProps({
-    scale: Number,
-    overlaySelector: String,
-  });
+defineProps({
+  overlaySelector: String,
+});
 
-  const colorBoxLeft = ref(randomHsl());
-  const colorBoxRight = ref(randomHsl());
-  const allAssetsLoaded = ref(false);
+const doorOpen = ref(false);
+const allAssetsLoaded = ref(false);
+
+function toggleDoor() {
+  doorOpen.value = !doorOpen.value
+  doorOpen.value ? console.log("door opened") : console.log("door closed")
+}
 </script>
 
 <template>
-  <a-scene
-    background="color: black;"
-    renderer="colorManagement: true;"
-    :webxr="`
-      requiredFeatures: local-floor;
-      referenceSpaceType: local-floor;
-      optionalFeatures: dom-overlay;
-      overlayElement: ${overlaySelector};
-    `"
-  >
-
-    <a-assets @loaded="allAssetsLoaded = true">
+<a-scene
+      background="color: black;"
+      renderer="colorManagement: true;"
+      :webxr="`
+        requiredFeatures: local-floor;
+        referenceSpaceType: local-floor;
+        optionalFeatures: dom-overlay;
+        overlayElement: ${overlaySelector};
+      `"
+>
+  <a-assets @loaded="allAssetsLoaded = true">
       <!--
         Title: VR Gallery
         Model source: https://sketchfab.com/3d-models/vr-gallery-1ac32ed62fdf424498acc146fad31f7e
         Model author: https://sketchfab.com/mvrc.art (Maxim Mavrichev)
         Model license: CC BY 4.0 ( https://creativecommons.org/licenses/by/4.0/ )
       -->
-      <a-asset-item  id="room" src="assets/vr_gallery.glb"></a-asset-item>
+      <a-asset-item  id="cabin" src="assets/small_cabin_house.glb"></a-asset-item>
     </a-assets>
+  <a-entity>
 
     <a-entity
       v-if="allAssetsLoaded"
-      gltf-model="#room"
-      rotation="0 90 0"
-      position="0 0 -5"
-      scale="1 1.1 1"
+      gltf-model="#cabin"
+      position="0 0 -1"
     >
-      <a-entity
-        geometry="primitive: plane; height: 2; width: 2;"
-        position="2 2 3.9"
-        rotation="-180 0 0"
-        life-like-automaton="genPerSec: 25;"
-      ></a-entity>
-
-      <a-entity
-        geometry="primitive: plane; height: 2; width: 2;"
-        position="2 2 -3.9"
-        life-like-automaton="genPerSec: 25; maxGen: 10; birthRule: 6,7,8; survivalRule: 4,5,6,7,8;"
-      ></a-entity>
-
-      <BoxColorChanging
-        id="box-left"
-        :scale="scale"
-        :color="colorBoxLeft"
-        position="7 0.5 -3"
-        @click="colorBoxRight = randomHsl()"
-      />
-
-      <BoxColorChanging
-        id="box-right"
-        :scale="scale"
-        :color="colorBoxRight"
-        position="7 0.5 3"
-        @click="colorBoxLeft = randomHsl()"
-      />
+      <a-entity light="type: ambient; color: #fff; intensity: 0.02;"></a-entity>
+      <a-entity light="type: point; color: #FFF; intensity: 0.02" position="0 1.5 -1"></a-entity>
     </a-entity>
 
-    <TheNavMesh />
+  </a-entity>
+  <!-- <a-entity>
 
-    <TheCameraRig />
 
-  </a-scene>
+
+    <a-plane position="0 1.5 -4" color="white" height="3" width="6"></a-plane>
+
+    <a-plane position="3 1.5 -1" color="white" height="3" width="6" rotation="0 -90 0">
+      <a-entity 
+        gltf-model="../assets/door.glb"
+        scale="0.01 0.01 0.01"
+        position="0 0.6 0"
+        clickable="color: red"
+        @click="toggleDoor"
+      ></a-entity>
+    </a-plane>
+
+    <a-plane position="-3 1.5 -1" color="white" height="3" width="6" rotation="0 90 0"></a-plane>
+
+    <a-plane position="0 1.5 2" color="white" height="3" width="6" rotation="0 180 0"></a-plane>
+
+    <a-plane position="0 3 -1" color="white" height="6" width="6" rotation="90 0 0"></a-plane>
+
+    <a-plane position="0 0 -1" color="white" height="6" width="6" rotation="-90 0 0"></a-plane>
+
+    <a-plane position="0 0 -1" color="white" height="4.5" width="4.5" rotation="-90 0 0" data-role="nav-mesh"
+      visible="false"></a-plane>
+  </a-entity> -->
+
+  <TheCameraRig />
+</a-scene>
 </template>
